1d32299b43de5ce3a13f264b3656ef2164846ccb
bluespace01 <frank.sha01@qq.com>
2024-12-18 10:26:13 +0800

    Update https_server.py and relevant js.

--- https_server.py
+++ https_server.py
@@ -1,20 +1,25 @@
 import http.server
 import ssl
+import os
 
 # 设置监听端口
 PORT = 9090
 
+# 切换当前工作目录到 dist 目录
+os.chdir("dist")
+
 # 设置服务器地址和目录
 Handler = http.server.SimpleHTTPRequestHandler
+
+# 创建 HTTPServer 实例
 httpd = http.server.HTTPServer(('0.0.0.0', PORT), Handler)
 
-# 加载证书和私钥文件
-httpd.socket = ssl.wrap_socket(
-    httpd.socket,
-    certfile="server.cert",
-    keyfile="server.key",
-    server_side=True
-)
+# 创建 SSL 上下文
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain(certfile="../ssl/localhost.pem", keyfile="../ssl/localhost-key.pem")
+
+# 将 SSL 上下文应用到服务器的 socket 上
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
 print(f"Serving HTTPS on https://0.0.0.0:{PORT}")
 httpd.serve_forever()
